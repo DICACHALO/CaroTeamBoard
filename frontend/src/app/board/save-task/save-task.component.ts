6577c0456afb2348bbd2dac29ed28558c1b4a6d5
Diana Chacon <dchacon871@unab.edu.co>
2021-08-30 16:39:56 -0500

    Feat: save-task

--- frontend/src/app/board/save-task/save-task.component.ts
+++ frontend/src/app/board/save-task/save-task.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import { BoardService } from '../../services/board.service';
+import { Router } from '@angular/router';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-save-task',
   templateUrl: './save-task.component.html',
-  styleUrls: ['./save-task.component.css']
+  styleUrls: ['./save-task.component.css'],
 })
 export class SaveTaskComponent implements OnInit {
+  registerData: any;
+  message: string = '';
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  durationInSeconds: number = 2;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private _boardService: BoardService,
+    private _router: Router,
+    private _snackBar: MatSnackBar
+  ) {
+    this.registerData = {};
   }
 
+  ngOnInit(): void {}
+
+  saveTask() {
+    if (!this.registerData.name || !this.registerData.description) {
+      console.log('Failed process: Imcomplete data');
+      this.message = 'Failed process: Imcomplete data';
+      this.openSnackBarError();
+      this.registerData = {};
+    } else {
+      this._boardService.saveTask(this.registerData).subscribe(
+        (res) => {
+          console.log(res);
+          this._router.navigate(['/listTask']);
+          this.message = 'Task create';
+          this.openSnackBarSuccesfull();
+          this.registerData = {};
+        },
+        (err) => {
+          console.log(err);
+          this.message = err.error;
+          this.openSnackBarError();
+        }
+      );
+    }
+  }
+
+  openSnackBarSuccesfull() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds * 1000,
+      panelClass: ['style-snackBarTrue'],
+    });
+  }
+
+  openSnackBarError() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds * 1000,
+      panelClass: ['style-snackBarFalse'],
+    });
+  }
 }
--- frontend/src/app/services/board.service.ts
+++ frontend/src/app/services/board.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BoardService {
+  private env: string;
 
-  constructor() { }
+  constructor(private _http: HttpClient, private _router: Router) {
+    this.env = environment.APP_URL;
+  }
+
+  saveTask(board: any) {
+    return this._http.post<any>(this.env + 'board/saveTask', board);
+  }
+
+  listTask() {
+    return this._http.get<any>(this.env + 'board/listTask');
+  }
+
+  updateTask(board: any) {
+    return this._http.put<any>(this.env + 'board/updateTask', board);
+  }
+
+  deleteTask(board: any) {
+    return this._http.delete<any>(this.env + 'board/deleteTask/' + board._id);
+  }
 }
